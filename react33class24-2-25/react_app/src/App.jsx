cf5075b91cdc9e57a708059f3233893feb570c19
sani <sz1769193@gmail.com>
2025-02-25 23:53:44 +0500

    reactapp

---
+++ react33class24-2-25/react_app/src/App.jsx
@@ -0,0 +1,13 @@
+import {Header} from './Components/Header/header'
+
+function App() {
+
+  return(
+    <>
+    <Header />
+    </>
+  )
+
+}
+
+export default App
---
+++ react33class24-2-25/react_app/src/Components/Header/header.jsx
@@ -0,0 +1,21 @@
+ export const Header = () => {
+
+    // debugging
+    // console.log("you are a header");
+
+    return(
+        <header>
+            <h1>LOGO</h1>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact us</li>
+                    <li>Services</li>
+                </ul>
+            </nav>
+        </header>
+    )
+
+}
+
